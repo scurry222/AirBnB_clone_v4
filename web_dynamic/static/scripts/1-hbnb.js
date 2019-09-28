4b0a8a66a0ddb53d6bde30c55783cdc2685f279a
scurry222 <scurry222@gmail.com>
2019-09-28 17:53:39 +0000

    setup for adding new element, removing debug line

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,13 +1,10 @@
 const dict = {};
-
-window.addEventListener('DOMContentLoaded', function() {
-    $('input#INPUT').click(function (event) {
-        console.log("we got inside func");
-        if ('amenity.id' !== dict.entries()) {
-            console.log("we got here");
-            dict.remove('amenity.id');
-        } else {
-            dict.push('amenity.id');
+$(document).ready(function() {
+    $('.popover :checkbox').change(function () {
+        if (this.checked) {
+                dict[$(this).attr('data-id')] = $(this).attr('data-name');
+            } else {
+            delete dict[$(this).attr('data-id')];
         }
     });
 });
