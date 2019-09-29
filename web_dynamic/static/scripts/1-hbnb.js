ec2d1ca3fcff2bf2062a1cc6bad09b5c60f5ae42
scurry222 <scurry222@gmail.com>
2019-09-29 15:38:43 +0000

    add selected amenities to h4 header

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -5,6 +5,10 @@ $(document).ready(function() {
                 dict[$(this).attr('data-id')] = $(this).attr('data-name');
             } else {
             delete dict[$(this).attr('data-id')];
+        } if (Object.keys(dict).length === 0) {
+            $('div.amenities h4').html('&nbsp');
+        } else {
+          $('div.amenities h4').text(Object.values(dict).join(', '));
         }
     });
 });
