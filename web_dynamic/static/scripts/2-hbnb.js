80e7c310e9ee9d2f657d7fdb018f18645fe1b33c
scurry222 <scurry222@gmail.com>
2019-09-29 18:16:36 +0000

    add api status configuration

---
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -0,0 +1,23 @@
+const dict = {};
+$(document).ready(function() {
+    $('.popover :checkbox').change(function () {
+        if (this.checked) {
+                dict[$(this).attr('data-id')] = $(this).attr('data-name');
+            } else {
+            delete dict[$(this).attr('data-id')];
+        } if (Object.keys(dict).length === 0) {
+            $('div.amenities h4').html('&nbsp');
+        } else {
+          $('div.amenities h4').text(Object.values(dict).join(', '));
+        }
+    });
+});
+window.addEventListener('load', function() {
+    $.ajax('http://0.0.0.0:5001/api/v1/status').done(function(data) {
+        if (data.status === 'OK') {
+            $('#api_status').addClass('available');
+        } else {
+            $('#api_status').removeClass('available');
+        }
+    });
+});
